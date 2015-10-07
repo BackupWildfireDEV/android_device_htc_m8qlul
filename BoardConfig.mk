bae442f78b59df5f01d9dc62e9658fdb1c8bf2fb
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-07 11:17:02 -0500

    msm8939: remove BOARD_SEPOLICY_UNION
    * all files found in BOARD_SEPOLICY_DIRS are now implicitly unioned
    
    Change-Id: If345f49a1e2f6ce769848ea02556305fd6fe45ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,20 +206,6 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-    bluetooth_loader.te \
-    device.te \
-    file.te \
-    file_contexts \
-    healthd.te \
-    property_contexts \
-    qseecomd.te \
-    surfaceflinger.te \
-    system.te \
-    system_app.te \
-    system_server.te \
-    wcnss_service.te
-
 # Video
 TARGET_HAVE_SIGNED_VENUS_FW := true
 
